277c46b5f58a6faaa3644fc94303a3f9dedc2b33
carloslimasis <carloslimasis@gmail.com>
2018-04-09 17:31:35 -0300

    Initial commit

---
+++ deck.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// Any variable of type "deck"
+// now gets acess to the
+// "print" method
+func (cards deck) print() {
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
